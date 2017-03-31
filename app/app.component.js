bc31ecff943fedfd6b06e3df8087a59a0b785218
Rocky Li <liqiuyi1991@gmail.com>
2017-03-31 00:27:04 -0500

    up to event binding

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './components/courses.components', './components/authors.components'], function(exports_1, context_1) {
+System.register(['angular2/core'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,28 +10,37 @@ System.register(['angular2/core', './components/courses.components', './componen
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, courses_components_1, authors_components_1;
+    var core_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
-            },
-            function (courses_components_1_1) {
-                courses_components_1 = courses_components_1_1;
-            },
-            function (authors_components_1_1) {
-                authors_components_1 = authors_components_1_1;
             }],
         execute: function() {
+            /* first project code */
+            //import {CoursesComponent} from './components/courses.components'; // import the component first to make it visible in directives
+            //import {AuthorsComponent} from './components/authors.components';
             AppComponent = (function () {
                 function AppComponent() {
+                    this.title = 'Angular App';
+                    this.imageUrl = 'http://lorempixel.com/400/200/';
+                    this.isActive = true;
                 }
+                AppComponent.prototype.onClick = function () {
+                    console.log('Clicked');
+                };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n                <h1>Hello Angular</h1>\n                <courses></courses>\n                <authors></authors>\n              ",
-                        directives: [courses_components_1.CoursesComponent, authors_components_1.AuthorsComponent] // any directives or components used inside this current component
+                        /* first project code */
+                        // template: `
+                        //             <h1>Hello Angular</h1>
+                        //             <courses></courses>
+                        //             <authors></authors>
+                        //           `,
+                        // directives: [CoursesComponent, AuthorsComponent] // any directives or components used inside this current component
+                        template: "\n                <h1> {{ title }} </h1> <!-- interpolation -->\n                <h1 [textContent] = \"title\"></h1> <!-- square equivalent -->\n\n                <img src=\"{{ imgageUrl }}\" />\n                <img [src]=\"imageUrl\" />\n                <img bind-src=\"imageUrl\" class=\"bind-src\" />\n                <!-- all of the above are property binding -->\n                <br/>\n                <p> ---------------------------------------- </p>\n                <br/>\n                <button class=\"btn btn-primary\" [class.active]=\"isActive\"> Class Binding </button> <!-- class binding, ps: bootstrap styling not working -->\n                <button class=\"btn btn-primary\" [style.backgroundColor]=\"isActive ? 'blue' : 'gray'\"> Style Binding </button>\n                <br/>\n                <p> ---------------------------------------- </p>\n                <br/>\n                <button (click)=\"onClick()\"> Event Binding </button>\n                <button on-click=\"onClick()\"> Event Binding </button>\n\n              "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,46 @@
 import {Component} from 'angular2/core';
-import {CoursesComponent} from './components/courses.components'; // import the component first to make it visible in directives
-import {AuthorsComponent} from './components/authors.components';
+/* first project code */
+//import {CoursesComponent} from './components/courses.components'; // import the component first to make it visible in directives
+//import {AuthorsComponent} from './components/authors.components';
 
 @Component({
     selector: 'my-app',
+    /* first project code */
+    // template: `
+    //             <h1>Hello Angular</h1>
+    //             <courses></courses>
+    //             <authors></authors>
+    //           `,
+    // directives: [CoursesComponent, AuthorsComponent] // any directives or components used inside this current component
+
     template: `
-                <h1>Hello Angular</h1>
-                <courses></courses>
-                <authors></authors>
-              `,
-    directives: [CoursesComponent, AuthorsComponent] // any directives or components used inside this current component
+                <h1> {{ title }} </h1> <!-- interpolation -->
+                <h1 [textContent] = "title"></h1> <!-- square equivalent -->
+
+                <img src="{{ imgageUrl }}" />
+                <img [src]="imageUrl" />
+                <img bind-src="imageUrl" class="bind-src" />
+                <!-- all of the above are property binding -->
+                <br/>
+                <p> ---------------------------------------- </p>
+                <br/>
+                <button class="btn btn-primary" [class.active]="isActive"> Class Binding </button> <!-- class binding, ps: bootstrap styling not working -->
+                <button class="btn btn-primary" [style.backgroundColor]="isActive ? 'blue' : 'gray'"> Style Binding </button>
+                <br/>
+                <p> ---------------------------------------- </p>
+                <br/>
+                <button (click)="onClick()"> Event Binding </button>
+                <button on-click="onClick()"> Event Binding </button>
+
+              `
 })
-export class AppComponent { }
+export class AppComponent { 
+  title = 'Angular App';
+  imageUrl = 'http://lorempixel.com/400/200/';
+
+  isActive = true;
+
+  onClick() {
+    console.log('Clicked');
+  }
+ }
